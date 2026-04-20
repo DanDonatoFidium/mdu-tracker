3783361e5046f9a3ce19d451046f7ba1c226958f
DanDonatoFidium <dandonato1975@gmail.com>
2026-04-20 08:07:28 -0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://yihytozgpezoqromatba.supabase.co';
-const supabaseAnonKey = 'eyJhbG...';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpaHl0b3pncGV6b3Fyb21hdGJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0MzkzMzMsImV4cCI6MjA5MjAxNTMzM30.Kejkb6SRg7Czrd55zoOxQdCzsr0Pw6huBug5uGzg2OA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
